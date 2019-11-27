818c5323ec47dfefc735d0300300f8900bb050eb
Arik Shapiro <aryesh@post.bgu.ac.il>
2019-11-27 10:38:00 +0200

    add cmake files

---
+++ cmake-build-debug/CMakeFiles/SPLFLIX.dir/DependInfo.cmake
@@ -0,0 +1,41 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/arik/CLionProjects/SPLFLIX/src/BaseAction.cpp" "/home/arik/CLionProjects/SPLFLIX/cmake-build-debug/CMakeFiles/SPLFLIX.dir/src/BaseAction.cpp.o"
+  "/home/arik/CLionProjects/SPLFLIX/src/ChangeActiveUser.cpp" "/home/arik/CLionProjects/SPLFLIX/cmake-build-debug/CMakeFiles/SPLFLIX.dir/src/ChangeActiveUser.cpp.o"
+  "/home/arik/CLionProjects/SPLFLIX/src/CreateUser.cpp" "/home/arik/CLionProjects/SPLFLIX/cmake-build-debug/CMakeFiles/SPLFLIX.dir/src/CreateUser.cpp.o"
+  "/home/arik/CLionProjects/SPLFLIX/src/DeleteUser.cpp" "/home/arik/CLionProjects/SPLFLIX/cmake-build-debug/CMakeFiles/SPLFLIX.dir/src/DeleteUser.cpp.o"
+  "/home/arik/CLionProjects/SPLFLIX/src/DuplicateUser.cpp" "/home/arik/CLionProjects/SPLFLIX/cmake-build-debug/CMakeFiles/SPLFLIX.dir/src/DuplicateUser.cpp.o"
+  "/home/arik/CLionProjects/SPLFLIX/src/Episode.cpp" "/home/arik/CLionProjects/SPLFLIX/cmake-build-debug/CMakeFiles/SPLFLIX.dir/src/Episode.cpp.o"
+  "/home/arik/CLionProjects/SPLFLIX/src/Exit.cpp" "/home/arik/CLionProjects/SPLFLIX/cmake-build-debug/CMakeFiles/SPLFLIX.dir/src/Exit.cpp.o"
+  "/home/arik/CLionProjects/SPLFLIX/src/GenreRecommenderUser.cpp" "/home/arik/CLionProjects/SPLFLIX/cmake-build-debug/CMakeFiles/SPLFLIX.dir/src/GenreRecommenderUser.cpp.o"
+  "/home/arik/CLionProjects/SPLFLIX/src/LengthRecommenderUser.cpp" "/home/arik/CLionProjects/SPLFLIX/cmake-build-debug/CMakeFiles/SPLFLIX.dir/src/LengthRecommenderUser.cpp.o"
+  "/home/arik/CLionProjects/SPLFLIX/src/Movie.cpp" "/home/arik/CLionProjects/SPLFLIX/cmake-build-debug/CMakeFiles/SPLFLIX.dir/src/Movie.cpp.o"
+  "/home/arik/CLionProjects/SPLFLIX/src/PrintActionsLog.cpp" "/home/arik/CLionProjects/SPLFLIX/cmake-build-debug/CMakeFiles/SPLFLIX.dir/src/PrintActionsLog.cpp.o"
+  "/home/arik/CLionProjects/SPLFLIX/src/PrintContentList.cpp" "/home/arik/CLionProjects/SPLFLIX/cmake-build-debug/CMakeFiles/SPLFLIX.dir/src/PrintContentList.cpp.o"
+  "/home/arik/CLionProjects/SPLFLIX/src/PrintWatchHistory.cpp" "/home/arik/CLionProjects/SPLFLIX/cmake-build-debug/CMakeFiles/SPLFLIX.dir/src/PrintWatchHistory.cpp.o"
+  "/home/arik/CLionProjects/SPLFLIX/src/RerunRecommenderUser.cpp" "/home/arik/CLionProjects/SPLFLIX/cmake-build-debug/CMakeFiles/SPLFLIX.dir/src/RerunRecommenderUser.cpp.o"
+  "/home/arik/CLionProjects/SPLFLIX/src/Session.cpp" "/home/arik/CLionProjects/SPLFLIX/cmake-build-debug/CMakeFiles/SPLFLIX.dir/src/Session.cpp.o"
+  "/home/arik/CLionProjects/SPLFLIX/src/User.cpp" "/home/arik/CLionProjects/SPLFLIX/cmake-build-debug/CMakeFiles/SPLFLIX.dir/src/User.cpp.o"
+  "/home/arik/CLionProjects/SPLFLIX/src/Watch.cpp" "/home/arik/CLionProjects/SPLFLIX/cmake-build-debug/CMakeFiles/SPLFLIX.dir/src/Watch.cpp.o"
+  "/home/arik/CLionProjects/SPLFLIX/src/Watchable.cpp" "/home/arik/CLionProjects/SPLFLIX/cmake-build-debug/CMakeFiles/SPLFLIX.dir/src/Watchable.cpp.o"
+  "/home/arik/CLionProjects/SPLFLIX/src/main.cpp" "/home/arik/CLionProjects/SPLFLIX/cmake-build-debug/CMakeFiles/SPLFLIX.dir/src/main.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "../."
+  "../include"
+  "../src"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ cmake-build-debug/CMakeFiles/SPLFLIX.dir/cmake_clean.cmake
@@ -0,0 +1,28 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/SPLFLIX.dir/src/BaseAction.cpp.o"
+  "CMakeFiles/SPLFLIX.dir/src/ChangeActiveUser.cpp.o"
+  "CMakeFiles/SPLFLIX.dir/src/CreateUser.cpp.o"
+  "CMakeFiles/SPLFLIX.dir/src/DeleteUser.cpp.o"
+  "CMakeFiles/SPLFLIX.dir/src/DuplicateUser.cpp.o"
+  "CMakeFiles/SPLFLIX.dir/src/Episode.cpp.o"
+  "CMakeFiles/SPLFLIX.dir/src/Exit.cpp.o"
+  "CMakeFiles/SPLFLIX.dir/src/GenreRecommenderUser.cpp.o"
+  "CMakeFiles/SPLFLIX.dir/src/LengthRecommenderUser.cpp.o"
+  "CMakeFiles/SPLFLIX.dir/src/Movie.cpp.o"
+  "CMakeFiles/SPLFLIX.dir/src/PrintActionsLog.cpp.o"
+  "CMakeFiles/SPLFLIX.dir/src/PrintContentList.cpp.o"
+  "CMakeFiles/SPLFLIX.dir/src/PrintWatchHistory.cpp.o"
+  "CMakeFiles/SPLFLIX.dir/src/RerunRecommenderUser.cpp.o"
+  "CMakeFiles/SPLFLIX.dir/src/Session.cpp.o"
+  "CMakeFiles/SPLFLIX.dir/src/User.cpp.o"
+  "CMakeFiles/SPLFLIX.dir/src/Watch.cpp.o"
+  "CMakeFiles/SPLFLIX.dir/src/Watchable.cpp.o"
+  "CMakeFiles/SPLFLIX.dir/src/main.cpp.o"
+  "SPLFLIX"
+  "SPLFLIX.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/SPLFLIX.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
